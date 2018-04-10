8ce8c5951756d9a28767642d048bdd5f34a6a200
Henry Barnacle <henrybarnacle@yahoo.com>
2018-04-10 10:03:16 -0400

    adding edit/ delete methods

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+//const db = mongoose.connect()
 const app = express();
+
 //map global to get rid of warning
 mongoose.Promise = global.Promise;
 
 //connect to mongoose
-mongoose.connect('mongodb://localhost/vidjot-dev', { 
+mongoose.connect('mongodb://henrybarnacle:henrybarnacle@ds139919.mlab.com:39919/vidjot', { 
 	useMongoClient: true
 })
 .then(() =>  console.log('MongoDB connected...'))
@@ -28,6 +31,8 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
+//method override middleware
+app.use(methodOverride('_method'));
 
 //index route
 app.get('/', (req, res) => {
@@ -40,11 +45,48 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
 	res.render('about');
 });
+//idea index page
+app.get('/ideas', (req,res) => {
+	Idea.find({}).sort({date: 'desc'}).then(ideas => {
+	res.render('ideas/index', {
+		ideas: ideas
+	});
+	});
+});
 
-//add idea route
+//add idea form
 app.get('/ideas/add', (req, res) => {
 	res.render('ideas/add');
 });
+
+//Edit Idea Form
+app.get('/ideas/edit/:id', (req, res) => {
+	Idea.findOne({
+		_id: req.params.id
+	}).then(idea => {
+		res.render('ideas/edit', {
+			idea: idea
+		});
+	});
+});
+
+
+
+//edit idea process
+app.put('/ideas/:id', (req, res) => {
+	Idea.findOne({
+		_id: req.params.id
+	}).then(idea => {
+		//new values
+		idea.title = req.body.title,
+		idea.details = req.body.details
+
+		idea.save().then(idea =>{
+			res.redirect('/ideas');
+		})
+		});
+	});
+
 
 //process form
 app.post('/ideas', (req, res) => {
@@ -62,10 +104,19 @@ app.post('/ideas', (req, res) => {
 			details: req.body.details
 		});
 	} else {
-		res.send('passed');
+		const newUser ={
+			title: req.body.title,
+			details: req.body.details
+		}
+		new Idea(newUser).save().then( idea => {
+			res.redirect('/ideas');
+		});
 	}
 
 });
+
+//Edit form process
+
 
 const port = 5000;
 
