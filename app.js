3af9fd7a4bae4106f37eff090101d8a5c06b3168
Henry Barnacle <henrybarnacle@yahoo.com>
2018-04-15 14:45:34 -0400

    adding user register and login capability

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
 //const db = mongoose.connect()
 const app = express();
+
+//load Routes
+
+const ideas = require('./routes/ideas');
+const users = require('./routes/users')
+
+//passport config
+require('./config/passport')(passport);
 
 //map global to get rid of warning
 mongoose.Promise = global.Promise;
@@ -14,113 +26,71 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://henrybarnacle:henrybarnacle@ds139919.mlab.com:39919/vidjot', { 
 	useMongoClient: true
 })
-.then(() =>  console.log('MongoDB connected...'))
-.catch(err => console.log(err));
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
-//load idea model
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
 
-// handlebars middleware
+
+// Handlebars Middleware
 app.engine('handlebars', exphbs({
-	defaultLayout: 'main'
+  defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
-//body parser middleware
-app.use(bodyParser.urlencoded({ extended: false}));
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//method override middleware
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Method override middleware
 app.use(methodOverride('_method'));
 
-//index route
+// Express session midleware
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+// Index Route
 app.get('/', (req, res) => {
-	const title = 'Welcome!!!';
-	res.render('index', {
-		title: title
-	});
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
 });
-//about route
+
+// About Route
 app.get('/about', (req, res) => {
-	res.render('about');
-});
-//idea index page
-app.get('/ideas', (req,res) => {
-	Idea.find({}).sort({date: 'desc'}).then(ideas => {
-	res.render('ideas/index', {
-		ideas: ideas
-	});
-	});
-});
-
-//add idea form
-app.get('/ideas/add', (req, res) => {
-	res.render('ideas/add');
-});
-
-//Edit Idea Form
-app.get('/ideas/edit/:id', (req, res) => {
-	Idea.findOne({
-		_id: req.params.id
-	}).then(idea => {
-		res.render('ideas/edit', {
-			idea: idea
-		});
-	});
+  res.render('about');
 });
 
 
-
-//edit idea process
-app.put('/ideas/:id', (req, res) => {
-	Idea.findOne({
-		_id: req.params.id
-	}).then(idea => {
-		//new values
-		idea.title = req.body.title,
-		idea.details = req.body.details
-
-		idea.save().then(idea =>{
-			res.redirect('/ideas');
-		})
-		});
-	});
-
-
-//process form
-app.post('/ideas', (req, res) => {
-	let errors = [];
-	if(!req.body.title) {
-		errors.push({text:'Please add a title'})
-	}
-	if(!req.body.details) {
-		errors.push({text:'Please add details'})
-	}
-	if(errors.length > 0 ) {
-		res.render('ideas/add', {
-			errors: errors,
-			title: req.body.title,
-			details: req.body.details
-		});
-	} else {
-		const newUser ={
-			title: req.body.title,
-			details: req.body.details
-		}
-		new Idea(newUser).save().then( idea => {
-			res.redirect('/ideas');
-		});
-	}
-
-});
-
-//Edit form process
-
+//use routes
+app.use('/ideas', ideas);
+app.use('/users', users);
 
 const port = 5000;
 
-app.listen(port, () => {
-	console.log(`Server started on port ${port}`);
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`);
 });
 
